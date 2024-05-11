fc93aa55a5f90fefe82d3c50862c9bb9f671e12a
LitFill <litfill@litfill.site>
2024-05-12 02:10:36 +0700

    feat(worker)!: use interval of an hour

--- service_worker.js
+++ service_worker.js
@@ -1,18 +1,19 @@
 console.info("worker deployed.");
 
-chrome.tabs.query({}, (tabs) => {
-	const tenDaysInMiliSeconds = 864_000_000; // 10 * 24 * 60 * 60 * 1000
+function autoCloseOldTabs() {
+	chrome.tabs.query({}, (tabs) => {
+		const tenDaysInMiliSeconds = 864_000_000; // 10 * 24 * 60 * 60 * 1000
 
-	for (const tab of tabs) {
-		const lastActivity = new Date(tab.lastActivityTime);
-		const now = new Date();
-		const timeSinceLastActivity = now.getTime() - lastActivity.getTime();
+		for (const tab of tabs) {
+			const lastActivity = new Date(tab.lastActivityTime);
+			const now = new Date();
+			const timeSinceLastActivity = now.getTime() - lastActivity.getTime();
 
-		if (timeSinceLastActivity > tenDaysInMiliSeconds) {
-			chrome.tabs.remove(tab.id);
+			if (timeSinceLastActivity > tenDaysInMiliSeconds) {
+				chrome.tabs.remove(tab.id);
+			}
 		}
-	}
-});
+	});
+}
 
-chrome.alarms.create("closeOldTabs", { periodInMinutes: 60 });
-
+setInterval(autoCloseOldTabs, 60 * 60 * 1000);
